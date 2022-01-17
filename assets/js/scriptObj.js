6b689060cbc12d4418c8f5901c64c6f6857d687a
Bretbret18 <bret.sperry@gmail.com>
2022-01-17 13:51:01 -0500

    single shoot event per round/ score posted and updated  in local storage

--- assets/js/scriptObj.js
+++ assets/js/scriptObj.js
@@ -17,14 +17,8 @@ let shootBtn = document.querySelector('#shoot-btn');
 // opponent DOM
 // Opponent round winner
 let opponentWinColumn = document.querySelector('#opponent-text')
-// opponent buttons for display
-const opponentBtns = document.querySelectorAll('.opponentBtn')
-// opponent play game button for display
-const opponentShootBtn = document.querySelector('#opponentShoot-btn');
 // opponent symbol placement
 const opponentResult = document.querySelector('#choice-box-opponent')
-// opponent div
-let opponentBox = document.querySelector('#opponent-div');
 // displays win/ lose column for round
 const opponentTextBox = document.querySelector('#opponent-text');
 // Opponent Box indicator
@@ -33,14 +27,10 @@ let opponentSide = document.querySelector('#Opponent-side');
 // Game DOM
 // resets page and game
 let playAgainBtn = document.querySelector('#play-again');
-// game symbols
-let childSymbols = document.querySelectorAll('.bi')
 // next round btn
 const nextRoundBtn = document.querySelector('#next-round-btn');
 // current round
 let round = document.querySelector('#round');
-// shows player vs opponent side
-const sideIndicators = document.querySelector('.side-indicator');
 // win/ lose ratio per game
 let resultIndicator = document.querySelector('#result-indicator');
 
@@ -49,11 +39,11 @@ const game = {
     round: [1, 2, 3],
     symbolArray: [
         {
-            rock: `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-gem" viewBox="0 0 16 16">
+            rock: `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-gem" viewBox="0 0 16 16">
     <path d="M3.1.7a.5.5 0 0 1 .4-.2h9a.5.5 0 0 1 .4.2l2.976 3.974c.149.185.156.45.01.644L8.4 15.3a.5.5 0 0 1-.8 0L.1 5.3a.5.5 0 0 1 0-.6l3-4zm11.386 3.785-1.806-2.41-.776 2.413 2.582-.003zm-3.633.004.961-2.989H4.186l.963 2.995 5.704-.006zM5.47 5.495 8 13.366l2.532-7.876-5.062.005zm-1.371-.999-.78-2.422-1.818 2.425 2.598-.003zM1.499 5.5l5.113 6.817-2.192-6.82L1.5 5.5zm7.889 6.817 5.123-6.83-2.928.002-2.195 6.828z"/>
   </svg>`},
         {
-            paper: `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60"
+            paper: `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"
   fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
   <path
       d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z" />
@@ -61,7 +51,7 @@ const game = {
       d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z" />
 </svg>`},
         {
-            scissors: `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60"
+            scissors: `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"
 fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
 <path
     d="M3.5 3.5c-.614-.884-.074-1.962.858-2.5L8 7.226 11.642 1c.932.538 1.472 1.616.858 2.5L8.81 8.61l1.556 2.661a2.5 2.5 0 1 1-.794.637L8 9.73l-1.572 2.177a2.5 2.5 0 1 1-.794-.637L7.19 8.61 3.5 3.5zm2.5 10a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0zm7 0a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z" />
@@ -96,6 +86,7 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
         round.innerHTML = roundIterator;
 
         nextRoundBtn.addEventListener('click', function (e) {
+            shootBtn.style.pointerEvents = 'auto';
             roundIterator++
             if (roundIterator >= 4) {
                 roundIterator = 3;
@@ -109,21 +100,16 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
             opponentWinColumn.innerHTML = '';
             opponentResult.style.color = 'black';
             playerResult.style.color = 'black';
-
         })
     },
     shootEvent: function () {
+
         shootBtn.addEventListener('click', function (e) {
+
             if (playerResult.firstElementChild == null) {
                 return null
-            };
-
-            let rockResult = game.symbolArray[0].rock;
-            let paperResult = game.symbolArray[1].paper;
-            let scissorsResult = game.symbolArray[2].scissors;
-
-            let gameArrayResults = [rockResult, paperResult, scissorsResult]
-
+            } 
+ 
             // highlight and display rock, paper, and scissors for opponent turn 
             setTimeout(() => {
                 opponentResult.innerHTML = game.symbolArray[0].rock;
@@ -135,6 +121,12 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
                 opponentResult.innerHTML = game.symbolArray[2].scissors;
             }, 2000);
             setTimeout(() => {
+
+                let rockResult = game.symbolArray[0].rock;
+                let paperResult = game.symbolArray[1].paper;
+                let scissorsResult = game.symbolArray[2].scissors;
+
+                let gameArrayResults = [rockResult, paperResult, scissorsResult]
 
                 opponentResult.innerHTML = gameArrayResults[getRandom()];
                 opponentResult.style.color = 'red';
@@ -165,8 +157,8 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
                 if (playerVal === 'bi bi-file-text' && opponentVal === 'bi bi-file-text') {
                     game.roundResultDraw()
                 }
-                 // Scissors
-                 if (playerVal === 'bi bi-scissors' && opponentVal === 'bi bi-file-text') {
+                // Scissors
+                if (playerVal === 'bi bi-scissors' && opponentVal === 'bi bi-file-text') {
                     game.roundResultPlayerWin()
                 }
                 if (playerVal === 'bi bi-scissors' && opponentVal === 'bi bi-gem') {
@@ -177,6 +169,8 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
                 }
             }, 3000);
 
+            shootBtn.style.pointerEvents = 'none';
+
             // get random symbol function
             function getRandom() {
                 return Math.floor(Math.random() * game.symbolArray.length)
@@ -185,6 +179,8 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
     },
     roundResultPlayerWin: function () {
         game.score.win++;
+        localStorage.setItem('record', `Win: ${game.score.win} Lose: ${game.score.lose} Draw: ${game.score.draw}`)
+        resultIndicator.innerHTML = `Win: ${game.score.win} Lose: ${game.score.lose} Draw: ${game.score.draw}`;
         playerWinColumn.innerHTML = 'Win!';
         playerWinColumn.style.color = 'green';
         opponentWinColumn.innerHTML = 'Lose!';
@@ -194,6 +190,8 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
     },
     roundResultPlayerLose: function () {
         game.score.lose++;
+        localStorage.setItem('record', `Win: ${game.score.win} Lose: ${game.score.lose} Draw: ${game.score.draw}`)
+        resultIndicator.innerHTML = `Win: ${game.score.win} Lose: ${game.score.lose} Draw: ${game.score.draw}`;
         playerWinColumn.innerHTML = 'Lose!';
         playerWinColumn.style.color = 'red';
         opponentWinColumn.innerHTML = 'Win!';
@@ -203,6 +201,8 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
     },
     roundResultDraw: function () {
         game.score.draw++;
+        localStorage.setItem('record', `Win: ${game.score.win} Lose: ${game.score.lose} Draw: ${game.score.draw}`)
+        resultIndicator.innerHTML = `Win: ${game.score.win} Lose: ${game.score.lose} Draw: ${game.score.draw}`;
         playerWinColumn.innerHTML = 'Draw!';
         opponentWinColumn.innerHTML = 'Draw!';
         console.log(game.score.win, game.score.lose, game.score.draw)
@@ -210,6 +210,7 @@ fill="currentColor" class="bi bi-scissors" viewBox="0 0 16 16">
     },
     playAgain: function () {
         playAgainBtn.addEventListener('click', function (e) {
+            localStorage.removeItem('record', `Win: ${game.score.win} Lose: ${game.score.lose} Draw: ${game.score.draw}`)
             location.reload();
         })
     }
@@ -220,11 +221,8 @@ game.shootEvent()
 game.getRound()
 game.playAgain()
 
-// TASKS LEFT TO COMPLETE: 
+// TASKS LEFT TO COMPLETE:
 // - Handle round function when roundResult is a draw
-// - Only allow shootBtn to be fired once per round
-// - Show game score on page
-// - set score to local storage
 // - Update page styles
 // - create alternate page that explains instructions of
 // game that can be opened or closed at any time
